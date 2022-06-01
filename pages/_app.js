a777d782bb205ede5daecd8c00478e97744ed725
Anh Nguyen <anhhn1@gmail.com>
2022-06-01 16:30:01 -0700

    fix linter warning/error

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
 import '../styles/globals.css'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script id="goog-anal-1" src="https://www.googletagmanager.com/gtag/js?id=G-XPLNM2KYK7" />
+      <Script id="goog-anal-2">
+        {`window.dataLayer = window.dataLayer || [];`}
+        {`function gtag(){dataLayer.push(arguments);`}
+        {`gtag('js', new Date());`}
+
+        {`gtag('config', 'G-XPLNM2KYK7');`}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
 
 export default function Document() {
   return (
@@ -13,14 +12,6 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <Script id="goog-anal-1" src="https://www.googletagmanager.com/gtag/js?id=G-XPLNM2KYK7" />
-        <Script id="goog-anal-2">
-          {`window.dataLayer = window.dataLayer || [];`}
-          {`function gtag(){dataLayer.push(arguments);`}
-          {`gtag('js', new Date());`}
-
-          {`gtag('config', 'G-XPLNM2KYK7');`}
-        </Script>
       </body>
     </Html>
   )
