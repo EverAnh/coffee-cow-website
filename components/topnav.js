5c38ac59b76dc07c8b2c2a72d1914b533b98d8fe
Anh Nguyen <anhhn1@gmail.com>
2022-06-06 18:02:25 -0700

    make mobile/responsive version of the navigation menu

--- components/topnav.js
+++ components/topnav.js
@@ -1,19 +1,83 @@
-import * as React from 'react';
-import Image from 'next/image';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
-import Toolbar from '@mui/material/Toolbar';
-import Tooltip from '@mui/material/Tooltip';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import LoginIcon from '@mui/icons-material/Login';
-import Avatar from '@mui/material/Avatar';
-import Stack from '@mui/material/Stack';
+import * as React from 'react'
+import Image from 'next/image'
+
+import AppBar from '@mui/material/AppBar'
+import Avatar from '@mui/material/Avatar'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Drawer from '@mui/material/Drawer'
+import IconButton from '@mui/material/IconButton'
+import Link from '@mui/material/Link'
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import ListItemButton from '@mui/material/ListItemButton'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import ListItemText from '@mui/material/ListItemText'
+import LoginIcon from '@mui/icons-material/Login'
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart'
+import Menu from '@mui/material/Menu'
+import MenuIcon from '@mui/icons-material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+import Stack from '@mui/material/Stack'
+import Toolbar from '@mui/material/Toolbar'
+import Tooltip from '@mui/material/Tooltip'
+import Typography from '@mui/material/Typography'
+
+const menuLinks = [
+  {
+    label: "Products",
+    href: "/#products"
+  },
+  {
+    label: "Testimonials",
+    href: "/#testimonials"
+  },
+  {
+    label: "Video",
+    href: "/#explainer"
+  },
+  {
+    label: "Countdown",
+    href: "/#countdown"
+  },
+  {
+    label: "Art",
+    href: "/art"
+  },
+];
 
 export default function TopNav({isFrontPage}) {
+
+  const [state, setState] = React.useState({
+    drawerOpen: false
+  });
+
+  const toggleDrawer = (open) => (event) => {
+    /*
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }*/
+    setState({ ...state, drawerOpen: open });
+  };
+
+  const drawerItems = () => (
+    <Box
+      sx={{ width: 240 }}
+      role="presentation"
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+    >
+      <List>
+        {menuLinks.map((e, index) => (
+          <ListItem key={e.label} disablePadding>
+            <ListItemButton component="a" href={e.href}>
+              <ListItemText primary={e.label} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
 
   return (
     <AppBar
@@ -21,78 +85,92 @@ export default function TopNav({isFrontPage}) {
         color={isFrontPage ? "transparent" : "primary"}
         elevation={0}
     >
-      <Toolbar sx={{py: 3}} id="top">
-        <Link
-          href="/#home"
-        >
+      <Toolbar sx={{alignItems: "center", justifyContent: "center"}}>
+        <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' }, alignItems: "center" }}>
+          <IconButton
+            size="large"
+            onClick={toggleDrawer(true)}
+            color="light"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Drawer
+            anchor="left"
+            open={state.drawerOpen}
+            onClose={toggleDrawer(false)}
+          >
+            {drawerItems()}
+          </Drawer>
+          <Typography
+            color="#d6d6d6"
+            variant="h5"
+          >
+            Coffee Cow
+          </Typography>
+          {/*
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorElNav}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'left',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'left',
+            }}
+            open={Boolean(anchorElNav)}
+            onClose={handleCloseNavMenu}
+            sx={{
+              display: { xs: 'block', md: 'none' },
+            }}
+          >
+            {menuLinks.map((ml) => (
+              <MenuItem onClick={handleCloseNavMenu}>
+                <Typography textAlign="center">{ml.label}</Typography>
+              </MenuItem>
+            ))}
+          </Menu>
+          */}
+        </Box>
+
+        <Link href="/#home" sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}>
           <Image
             src="/images/logo_cowface.png"
             alt="Coffee Cow Logo"
             width="62"
             height="32"
-            position="relative"
-            sx={{ flexShrink: 0 }}
           />
         </Link>
-        <Box sx={{ flexGrow: 1 }}>
-          <Link
-            variant="button"
-            href="/#products"
-            color="#d6d6d6"
-            fontSize="large"
-            sx={{ my: 1, ml: 3, mr: 1.5 }}
-          >
-            Products
-          </Link>
-          <Link
-            variant="button"
-            href="/#testimonials"
-            color="#d6d6d6"
-            fontSize="large"
-            sx={{ my: 1, mx: 1.5 }}
-          >
-            Testimonials
-          </Link>
-          <Link
-            variant="button"
-            href="/#explainer"
-            color="#d6d6d6"
-            fontSize="large"
-            sx={{ my: 1, mx: 1.5 }}
-          >
-            Video
-          </Link>
-          <Link
-            variant="button"
-            href="/#countdown"
-            color="#d6d6d6"
-            fontSize="large"
-            sx={{ my: 1, mx: 1.5 }}
-          >
-            Countdown
-          </Link>
-          <Link
-            variant="button"
-            href="/art"
-            color="#d6d6d6"
-            fontSize="large"
-            sx={{ my: 1, mx: 1.5 }}
-          >
-            Art
-          </Link>
+
+        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+          {menuLinks.map((ml) => (
+            <Link
+              variant="button"
+              key={ml.label}
+              href={ml.href}
+              color='#d6d6d6'
+              sx={{ my: 0, mx: 1.5, textDecoration: 'none' }}
+            >
+              {ml.label}
+            </Link>
+          ))}
         </Box>
-        <Stack direction="row" spacing={2} sx={{ alignSelf: 'flex-end' }}>
-          <Tooltip title="This button is fake." arrow>
-            <Avatar sx={{ bgcolor: "#222", width: "48px", height: "48px" }}>
-              <LoginIcon fontSize="medium" />
-            </Avatar>
-          </Tooltip>
-          <Tooltip title="This button is fake." arrow>
-            <Avatar sx={{ bgcolor: "#222", width: "48px", height: "48px" }}>
-              <ShoppingCartIcon fontSize="medium" />
-            </Avatar>
-          </Tooltip>
-        </Stack>
+        <Box>
+          <Stack direction="row" spacing={1} sx={{ alignSelf: 'flex-end', justifyContent: 'center' }}>
+            <Tooltip title="This button is fake." arrow>
+              <Avatar sx={{ bgcolor: "#222" }}>
+                <LoginIcon color="light" />
+              </Avatar>
+            </Tooltip>
+            <Tooltip title="This button is fake." arrow>
+              <Avatar sx={{ bgcolor: "#222" }}>
+                <ShoppingCartIcon color="light" />
+              </Avatar>
+            </Tooltip>
+          </Stack>
+        </Box>
       </Toolbar>
     </AppBar>
   );
